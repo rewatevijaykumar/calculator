f329e2e6e4a53c0443704c047195b2548c2aff09
rewatevijaykumar <rewatevijaykumar@gmail.com>
2023-11-16 15:26:28 +0530

    flask ui calculator

--- app_flask.py
+++ app_flask.py
@@ -5,11 +5,37 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])  # render homepage
 def home():
-    return 'home page'
+    return render_template('index.html')
+
+
+@app.route('/math', methods=['POST'])  # for calling api
+def calculator():
+    if request.method == 'POST':
+        operation = request.form['operation']
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+
+        if operation == 'add':
+            res = num1 + num2
+            result = f'sum of {str(num1)} and {str(num2)} is {str(res)}'
+
+        if operation == 'subtract':
+            res = num1 - num2
+            result = f'difference of {str(num1)} and {str(num2)} is {str(res)}'
+
+        if operation == 'multiply':
+            res = num1 * num2
+            result = f'product of {str(num1)} and {str(num2)} is {str(res)}'
+
+        if operation == 'divide':
+            res = num1 / num2
+            result = f'division of {str(num1)} and {str(num2)} is {str(res)}'
+
+        return render_template('index.html', result=result)
 
 
 @app.route('/api', methods=['POST'])  # for calling api
-def calculator():
+def calculator_api():
     if request.method == 'POST':
         operation = request.json['operation']
         num1 = int(request.json['num1'])
